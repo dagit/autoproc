19d70b8273ef2f19838c0840f8e58ed434aac929
gwern0 <gwern0@gmail.com>
2008-03-19 22:02:17 +0000

    Run.hs: pull the message out to its own function

--- Autoproc/Run.hs
+++ Autoproc/Run.hs
@@ -17,12 +17,16 @@ showProcmailrc ps = showVars (variables) ++ "\n\n" ++
                     "############################\n\n" ++
                     (ps >>= showLn)
 
+autoprocMessage :: IO ()
+autoprocMessage =  putStr $ unlines ["#.procmailrc",
+                           "#  Automatically generated procmail recipes by Autoproc.",
+                           "#  To find out more about Autoproc visit:",
+                           "#    'http://www.codersbase.com/Autoproc'",
+                           "#  To fetch the latest version of autoproc with Darcs:",
+                           "#    'darcs get http://projects.codersbase.com/repos/autoproc'",
+                           "",
+                           ""]
+
 autoprocMain :: Writer [CExp] a -> IO ()
-autoprocMain rules = do putStr "#.procmailrc \
-                              \ #  Automatically generated procmail recipes by Autoproc. \
-                              \ #  To find out more about Autoproc visit: \
-                              \ #    http://www.codersbase.com/Autoproc \
-                              \ #  To fetch the latest version of autoproc with darcs: \
-                              \ #    darcs get http://projects.codersbase.com/repos/autoproc \
-                               \n\n"
+autoprocMain rules = do autoprocMessage
                         putStrLn $ showProcmailrc $ concatMap generate $ execWriter rules
